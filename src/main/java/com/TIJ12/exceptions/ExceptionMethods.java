b8e4e9e21430ceeb4e74ffc7fdef5eb0d435aa54
foxliu <foxliu18@163.com>
2019-12-03 15:50:44 +0800

    exceptions

---
+++ src/main/java/com/TIJ12/exceptions/ExceptionMethods.java
@@ -0,0 +1,18 @@
+package com.TIJ12.exceptions;
+
+import static com.util.Print.print;
+
+public class ExceptionMethods {
+    public static void main(String[] args){
+        try {
+            throw new Exception("My Exception");
+        } catch (Exception e){
+            print("Caught Exception");
+            print("getMessage(): " + e.getMessage());
+            print("getLocalizedMessage(): " + e.getLocalizedMessage());
+            print("toString(): " + e);
+            print("printStackTrace(): ");
+            e.printStackTrace(System.out);
+        }
+    }
+}
